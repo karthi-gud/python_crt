a04ae67fbebcddea6625b8fc8ee28fa72acb21be
karthikeyagudapati <166837767+karthikeyagudapati@users.noreply.github.com>
2025-06-12 12:21:45 +0530

    VERSION-1

--- 10-1125/2home work.py
+++ 10-1125/2home work.py
@@ -4,11 +4,11 @@
 #password should have atleast
 #3 upper case alpha
 #3 lower case alpha 
-##3 spl charecters
-##and 1 number 
-#password length should not be less than 10
-name = input("Enter your name: ")
+#3 spl charecters
+#and 1 number 
+#password length should not be less than 10
 
+name = input("Enter your name: ")
 
 contact = input("Enter your contact number: ")
 if contact.isdigit() and len(contact) == 10:
@@ -16,11 +16,21 @@ if contact.isdigit() and len(contact) == 10:
     if "@" in email and "." in email:
         password = input("Enter your password: ")
         
-        upper = sum(1 for c in password if c.isupper())
-        lower = sum(1 for c in password if c.islower())
-        digit = sum(1 for c in password if c.isdigit())
-        special = sum(1 for c in password if not c.isalnum())
-        
+        upper = 0
+        lower = 0
+        digit = 0
+        special = 0
+
+        for c in password:
+            if c.isupper():
+                upper += 1
+            elif c.islower():
+                lower += 1
+            elif c.isdigit():
+                digit += 1
+            elif not c.isalnum():
+                special += 1
+
         if len(password) >= 10 and upper >= 3 and lower >= 3 and special >= 3 and digit >= 1:
             print("All inputs are valid!")
         else:
